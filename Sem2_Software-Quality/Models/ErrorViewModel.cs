65a1f08768d3c9f5f092a2ccdf20e3c013d87add
christofferPerch <cph-cn332@cphbusiness.dk>
2024-09-06 09:31:24 +0200

    Add project files.

---
+++ Sem2_Software-Quality/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sem2_Software_Quality.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
